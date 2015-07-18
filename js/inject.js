a9ffb5e80edd71a6b4c1d0f55954a3a68020aba6
Calv Collins <calvcoll@gmail.com>
2015-07-18 20:28:49 +0100

    added a skin and code now works on page change

--- js/inject.js
+++ js/inject.js
@@ -1,39 +1,60 @@
-$(document.body).ready(function() {
-    if (location.href.indexOf("inbox") >= 0) {
-        var typedName = "";
-        var refreshAnswerBoxes = function() {
-            var questions = $('.inbox-box');
-            var question_users = $('.answerbox--question-user');
-            for (var i=0; i<questions.length; i++) {
-                var question = questions[i];
-                var question_user = question_users[i];
+var onHead = function() {
+    $(document.head).ready(function() {
+        //Add RS Modified
+        console.log("My head is ready " + location.href);
+        $(document.head).append("<link rel='stylesheet' href=\"" + chrome.extension.getURL('/css/retrospring-modified.css') + "\"/>");
+    });
+};
+var onBody = function () {
+    $(document.body).ready(function() {
+        console.log("My body is ready " + location.href);
 
-                var user = question_user.innerText.split('asked')[0];
-                console.log(user);
+        if (location.href.indexOf("inbox") >= 0) {
+            var typedName = "";
+            var refreshAnswerBoxes = function() {
+                var questions = $('.inbox-box');
+                var question_users = $('.answerbox--question-user');
+                for (var i=0; i<questions.length; i++) {
+                    var question = questions[i];
+                    var question_user = question_users[i];
+                    var user = question_user.innerText.split('asked')[0];
 
-                if (user.toLowerCase().indexOf(typedName.toLowerCase()) >= 0) {
-                    $(question).show();
+                    if (user.toLowerCase().indexOf(typedName.toLowerCase()) >= 0) {
+                        $(question).show();
+                    }
+                    else {
+                        $(question).hide(400);
+                    }
                 }
-                else {
-                    $(question).hide(400);
-                }
-            }
-        };
+            };
 
-        var authorForm = $('.form-control#author');
-        authorForm.keypress(function(event){
-            typedName = event.currentTarget.value + String.fromCharCode(event.keyCode);
-            //console.log(typedName + " press");
-            refreshAnswerBoxes();
-        });
-
-        authorForm.keyup(function(event){
-            if (event.keyCode == 8) {
-                //typedName = typedName.split(typedName[typedName.length-1])[0];
-                typedName = event.currentTarget.value;
-                //console.log(typedName + " down");
+            var authorForm = $('.form-control#author');
+            authorForm.keypress(function(event){
+                typedName = event.currentTarget.value + String.fromCharCode(event.keyCode);
                 refreshAnswerBoxes();
-            }
-        });
-    }
+            });
+
+            authorForm.keyup(function(event){
+                if (event.keyCode == 8) {
+                    typedName = event.currentTarget.value;
+                    refreshAnswerBoxes();
+                }
+            });
+        }
+    });
+};
+
+/* Loads on page change - required because of Rails partials */
+var onload = function() {
+    onHead();
+    onBody();
+};
+
+$(document).on('page:load' ,function() {
+    onload();
 });
+
+// Runs for first page load
+onload();
+
+/* End of on load stuffs */
